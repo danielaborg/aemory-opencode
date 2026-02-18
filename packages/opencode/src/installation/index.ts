e6ce2e7ebe5332ba3e76e4e8d95621b5f71d41f2
Ariane Emory <ariane.emory@protonmail.com>
2026-02-17 20:47:06 -0500

    Apply finishing touches: theme, VERSION, SemVer fix, channel

--- packages/opencode/src/installation/index.ts
+++ packages/opencode/src/installation/index.ts
@@ -189,7 +189,7 @@ export namespace Installation {
     await $`${process.execPath} --version`.nothrow().quiet().text()
   }
 
-  export const VERSION = typeof OPENCODE_VERSION === "string" ? OPENCODE_VERSION : "local"
+  export const VERSION = typeof OPENCODE_VERSION === "string" ? OPENCODE_VERSION : "2026-02-17-17-11"
   export const CHANNEL = typeof OPENCODE_CHANNEL === "string" ? OPENCODE_CHANNEL : "local"
   export const USER_AGENT = `opencode/${CHANNEL}/${VERSION}/${Flag.OPENCODE_CLIENT}`
 
